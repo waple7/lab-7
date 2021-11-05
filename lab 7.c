b11e924967d50ab894a0954330c8ee8b66def2d4
waple7 <91288621+waple7@users.noreply.github.com>
2021-11-05 09:52:28 +0300

    Update lab 7.c

--- lab 7.c
+++ lab 7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-enum music {
+enum music { // перечисление
     classics,
     pop,
     rock,
@@ -10,14 +10,14 @@ enum music {
     electronics
 };
 
-struct coordinates {
+struct Triangle {
     int point_1[2];
     int point_2[2];
     int point_3[2];
 
 };
 
-struct unification {
+struct Card_reader { //объединение
     unsigned included: 1;
     unsigned sd_card: 1;
     unsigned compact_flash_card: 1;
@@ -25,8 +25,8 @@ struct unification {
 
 };
 
-union unity {
-    struct unification comprise;
+union unity {   // объединение исп одно и то же расположение памяти.размер объедин равен размеру нибольшь члена
+    struct Card_reader comprise; //вкл
     int write;
 
 };
@@ -34,30 +34,30 @@ union unity {
 int main() {
     enum music value;
     value = rock;
-    printf("number: %d\n", rock + 1);
-    struct coordinates triangle = {
+    printf("number: %d\n", rock + 1); // 3 по счету
+    struct Triangle triangle = {
             .point_1={0, 0},
             .point_2={0, 10},
             .point_3={10, 0}
 
     };
-    int square = (triangle.point_2[1] * triangle.point_3[0]) / 2;
-    printf("square triangle: %d\n", square);
+    int square;
+    square=(triangle.point_2[1] * triangle.point_3[0]) / 2;
+    printf("square triangle: %d\n", square); // площадь треугольника
 
 
     union unity arg;
-    int contain;
+    int contain; // содерж
     scanf("%x", &contain);
     arg.write = contain;
 
 
-    printf("values:");
-    printf("%d ", arg.comprise.included);
-    printf("%d ", arg.comprise.sd_card);
-    printf("%d ", arg.comprise.compact_flash_card);
-    printf("%d ", arg.comprise.MemoyStick_card);
+    printf("included:%d\n", arg.comprise.included);
+    printf("sd_card:%d\n", arg.comprise.sd_card);
+    printf("compact_flash_card:%d\n", arg.comprise.compact_flash_card);
+    printf("MemoyStick_card:%d\n", arg.comprise.MemoyStick_card);
 
 
     return 0;
-    
+
 }
